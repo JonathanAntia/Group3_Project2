c1bee413c0398eb6b36a964550bd23394478be92
JonathanAntia <a_jon12@yahoo.com>
2020-09-04 15:30:08 -0500

    function to calculate scores

--- static/js/scores_function.py
+++ static/js/scores_function.py
@@ -1,7 +1,20 @@
-def scores (w_sales, w_crime, w_schools, w_acreage, w_sqft, w_flood, w_change, df):
+def scores (dictionaryOfUserInput):
+budget = dictionaryOfUserInput[budget]
+W_sales = dictionaryOfUserInput[sales]
+W_crime = dictionaryOfUserInput[crime]
+w_schools = dictionaryOfUserInput[schools]
+w_acreage = dictionaryOfUserInput[acreage]
+w_SQ_FT = dictionaryOfUserInput[sqft]
+w_flood = dictionaryOfUserInput[Flood]
+w_change = dictionaryOfUserInput[change]
+
+# call Adriana's function to query the database and create a dataframe
+df = Adrianasbudgetfuction(budget)
+
 # Calculate scores for each address.
 total_weights=w_sales+w_crime+w_schools+w_acreage+w_SQ_FT+w_flood+w_change
 
+# Add calculated scores to the dataframe
 df["Sales Index_W"]=w_sales*df['Sales Index']
 df['Crime Index_W']= w_crime*df['Crime Index']
 df["School Rating Index_W"]=w_schools*df['School Rating Index']
@@ -10,6 +23,7 @@ df["SQ_FT_Index_W"]= w_SQ_FT*df['SQ_FT Index']
 df["Flood Risk Index_W"]=w_flood*df['Flood Risk Index']
 df['Valuation Index_W']= w_change*df['Valuation Index']
 
+# Calculate total score per row
 df["Score"]=round((w_sales*df['Sales Index']-
                                    w_crime*df['Crime Index']+
                                    w_schools*df['School Rating Index']+
@@ -17,9 +31,11 @@ df["Score"]=round((w_sales*df['Sales Index']-
                                    w_SQ_FT*df['SQ_FT Index']-
                                    w_flood*df['Flood Risk Index']+
                                    w_change*df['Valuation Index'])/total_weights,2)
-max=homes_less_than_1M["Score"].max()
-min=homes_less_than_1M["Score"].min()
-max=homes_less_than_1M["Score"]=(homes_less_than_1M["Score"]-min)/(max-min)*100
+
+# convert the score to percentage and scale them
+max=df["Score"].max()
+min=df["Score"].min()
+max=df["Score"]=(df["Score"]-min)/(max-min)*100
 
 # group parameters by neighborhood name
 neighborhood_group = parameter_and_score.groupby(["NEIGHBORHOOD"]).mean()
@@ -32,6 +48,6 @@ max=neighborhood_group['Valuation Index'].max()
 min=neighborhood_group['Valuation Index']=(neighborhood_group['Valuation Index']-min)/(max-min)*100
 
 # sort scores
-rank_all = neighborhood_group.sort_values('Score',ascending=False)
+ranked_neighborhoods = neighborhood_group.sort_values('Score',ascending=False)
 
-return rank_all.head()
+return ranked_neighborhoods.head()
