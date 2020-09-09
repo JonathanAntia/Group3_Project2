03ba1d36f6a746e4e771cf2a2e2cc3d809762876
Adriana <adrianaovalle@live.com>
2020-09-09 00:43:51 -0500

    updated app.py and SQL_for_Ranking to include all data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 def SQL_Pull(budget):
-    #Import Dependencies
+    # Import Dependencies
+    # Python SQL toolkit and Object Relational Mapper
     import sqlalchemy
     from sqlalchemy.ext.automap import automap_base
     from sqlalchemy.orm import Session
@@ -7,22 +8,23 @@ def SQL_Pull(budget):
     from key import url
     import numpy as np
     import pandas as pd
-
-    #Connect to PostgreSQL
+    
     #Create the engine
     engine = create_engine(url)
-
+    
     # reflect an existing database into a new model
     Base = automap_base()
-
+    
     # reflect the tables
     Base.prepare(engine, reflect=True)
+    
     session=Session(engine)
-
-    #Using Pandas for Data Analysis
+    
+    # Using Pandas for Data Analysis
     #  Read the appraisal Table
     appraisal=pd.read_sql_table('appraisal',engine)
-
+    
+    
     # Calculate % of change of value between 2018 and 2019
     appraisal_2018=appraisal.loc[appraisal.tax_year==2018,:]
     appraisal_2019=appraisal.loc[appraisal.tax_year==2019,:]
@@ -30,25 +32,19 @@ def SQL_Pull(budget):
     appraisal_df['pct_value_change']=(appraisal_df['total_appraised_value_2019']-appraisal_df['total_appraised_value_2018'])\
                                   /appraisal_df['total_appraised_value_2018']*100
     results_df=appraisal_df[['id_2019','account','total_appraised_value_2019', 'pct_value_change']]
-    results_df=results_df.rename(columns={'id_2019':'id'})
-
-    # Filter by budget on year 2019
-    results_df=results_df.loc[results_df.total_appraised_value_2019<=budget,:]
-
-    #Read properties table and merge to results
+    
+    #Read properties table
     properties_df=pd.read_sql_table('properties',engine)
     results_df=pd.merge(results_df,properties_df,on="account")
-    del results_df['latitude']
-    del results_df['longitude']
     del results_df['address']
-
+    
     #Read crime table and merge to results
     crime_df=pd.read_sql_table('crime',engine)
     crime_df=crime_df.rename(columns={'Zip_Code':'Zip_code'})
     crime_aggr=crime_df.groupby(['Zip_code']).count()['Offense_Count']
     crime_aggr_df=pd.DataFrame(crime_aggr)
     results_df=pd.merge(results_df,crime_aggr_df,on="Zip_code")
-
+    
     #Read property_school table and merge to results
     property_school_df=pd.read_sql_table('property_school',engine)
     results_df=pd.merge(results_df,property_school_df,on="account")
@@ -57,35 +53,42 @@ def SQL_Pull(budget):
     school_df=pd.read_sql_table('school',engine)
     results_df=pd.merge(results_df,school_df,on=['school_id','school_type'])
 
-    #Read flood_zone table and merge to results
-    # flood_zone_df=pd.read_sql_table('flood_zone',engine)
+    
     #Add flood ranking
     #3- High Risk
     #2 - Medium Risk
     #1- Low Risk
-    results_df['flood_risk']=np.where(results_df['flood_description']=='AREA OF MINIMAL FLOOD HAZARD',1," ")
+    results_df['flood_risk']=np.where(results_df['flood_description']=='AREA OF MINIMAL FLOOD HAZARD',1,3)
     results_df.loc[(results_df['flood_description']=='0.2 PCT ANNUAL CHANCE FLOOD HAZARD'),'flood_risk']=2
     results_df.loc[(results_df['flood_description']=='FLOODWAY'),'flood_risk']=3
-    results_df.loc[(results_df['flood_description']=='High-Risk Flood Zone'),'flood_risk']=3
     del results_df['flood_description']
     del results_df['name']
     del results_df['address']
     del results_df['city']
     del results_df['zip_code']
     del results_df['district_id']
-    del results_df['latitude']
-    del results_df['longitude']
-
-    #Count the house sale per neighborhood in 2019 and merge results
-    sales2019=results_df.loc[results_df.new_owner_date>'2018-12-31']
+    del results_df['latitude_y']
+    del results_df['longitude_y']
+    results_df=results_df.rename(columns={'latitude_x':'latitude', 'longitude_x':'longitude'})
+    
+    
+    # Count sales in 2019
     results_df['sales2019']=np.where(results_df['new_owner_date']>'2018-12-31',1,0)
-    sales=results_df.groupby('neighborhood_code')['sales2019'].count()
+    sales=results_df.groupby('neighborhood_code')['sales2019'].sum()
     sales=pd.DataFrame(sales)
     sales=sales.rename(columns={'sales2019':'sales_neighborhood_2019'})
     results_df=pd.merge(results_df,sales, on="neighborhood_code")
     del results_df['sales2019']
+    
+    #  Read the neighborhoods Table
+    neighborhoods_df=pd.read_sql_table('neighborhoods',engine)
+    results_df=pd.merge(results_df,neighborhoods_df,on='neighborhood_code')
+    
+    # Filter by budget on year 2019
+    results_df=results_df.loc[results_df.total_appraised_value_2019<=budget,:]
+    results_df.info()
     print('computation completed')
-    return (results_df)  
+    return (results_df) 
 
 #Run function SQL_Pull
-SQL_Pull(100000)
+SQL_Pull(1000000) 
