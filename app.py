55eab2e5edb5e3d03e37496916ad1578592bf1a3
Ellenrud84 <ellenrud84@gmail.com>
2020-09-10 15:32:24 -0500

    updated all python files to properly link modules and app.py to properly send data table to index.html through flask server.

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ import numpy as np
 import pandas as pd
 from flask import (Flask, render_template, jsonify, request, redirect)
 from flask_sqlalchemy import SQLAlchemy
-import processInputs 
-from userInputs import default_inputs
+import json
+from user_inputs import default_inputs
+import pull
+import processInputs
 
+
+####### INITIATE FLASK APP #########################
 app= Flask(__name__)
 
-
-
-# @app.route("/jsondata")
-# def jsondata():
-
-#    return jsonify(top5hoods)
-
-
-@app.route('/',  methods = ("POST", "GET"))
+###### DEFINE HOME ROUTE #################################
+@app.route('/')
 def home():
- return render_template('index.html', )
+    ##############CALL ON SCORES MODULE FROM processInputs WITH DEFAULT INPUTS######################################
+    data = processInputs.scores(default_inputs)
+    ########## RETURNS AT HOME ROUTE: #####################
+    return render_template('index.html', table=[data.to_html(classes='data')], titles=data.columns.values)
 
+############# FLASK CLOSING CODE ###################    
 if __name__ =='__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
-$('#ex1').bootstrapSlider({});
+// $('#ex1').bootstrapSlider({});
 function showTable (){
-    const jsonfile= "../../top5hoods.json";
+    const jsonfile= "top5hoods.json";
     d3.json(jsonfile).then(
         function(data){
             console.log(data);
